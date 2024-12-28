8a871b429dbaf4b904c132884a8d0e23a4865478
MilijanTeacher <135967376+MilijanTeacher@users.noreply.github.com>
2024-12-28 12:00:56 +0100

    Final JS

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
+let signs = ['+', '-', '*']
+let container_main = document.querySelector('.main')
+let container_start = document.querySelector('.start')
+let container_start_h3 = container_start.querySelector('h3')
 let question_field = document.querySelector('.question')
 let answer_buttons = document.querySelectorAll('.answer')
-let container_h3 = document.querySelector('.container_h3')
+let start_button = document.querySelector('.start-btn')
+
+
+let cookie = false
+let cookies = document.cookie.split('; ')
+for (let i = 0; i < cookies.length; i += 1) {
+    if (cookies[i].split('=')[0] == 'numbers_high_score') {
+        cookie = cookies[i].split('=')[1]
+        break
+    }
+}
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+if (cookie) {
+    let data = cookie.split('/')
+    container_start_h3.innerHTML = `<h3>Last time you gave ${data[1]} correct answers out of ${data[0]}. Accuracy is ${Math.round(data[1] * 100 / data[0])}%.</h3>`
+}
+
+
 
 
 function randint(min, max) {
     return Math.round(Math.random() * (max - min) + min)
-} 
+}
 
 
-let signs = ['+', '-', '*', '/']
 function getRandomSign() {
-    return signs[randint(0, 3)]
+    return signs[randint(0, 2)]
 }
-
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
-}
-
-
 
 
 class Question {
@@ -37,15 +56,14 @@ class Question {
         if (sign == '+') { this.correct = a + b }
         else if (sign == '-') { this.correct = a - b }
         else if (sign == '*') { this.correct = a * b }
-        else if (sign == '/') { this.correct = a / b }
         this.answers = [
-            randint(this.correct - 15, this.correct - 1),
-            randint(this.correct - 15, this.correct - 1),
+            randint(this.correct - 20, this.correct - 1),
+            randint(this.correct - 20, this.correct - 1),
             this.correct,
-            randint(this.correct + 1, this.correct + 15),
-            randint(this.correct + 1, this.correct + 15),
+            randint(this.correct + 1, this.correct + 20),
+            randint(this.correct + 1, this.correct + 20),
         ]
-        shuffle(this.answers)
+        shuffle(this.answers);
     }
 
 
@@ -58,20 +76,29 @@ class Question {
 }
 
 
-
-
-let correct_answers_given = 0
-let total_answers_given = 0
 let current_question
-current_question = new Question()
-current_question.display()
+let correct_answers_given
+let total_answers_given
+start_button.addEventListener('click', function() {
+    container_main.style.display = 'flex'
+    container_start.style.display = 'none'
+    current_question = new Question()
+    current_question.display()
 
 
-setTimeout(function() {
-container_h3.innerHTML = 
-`You gave ${correct_answers_given} correct answers out of ${total_answers_given}. 
-Accuracy is ${Math.round(correct_answers_given * 100 / total_answers_given)}%.`
+    correct_answers_given = 0
+    total_answers_given = 0
+
+
+    setTimeout(function() {
+        let new_cookie = `numbers_high_score=${total_answers_given}/${correct_answers_given}; max-age=10000000000`
+        document.cookie = new_cookie
+        
+container_main.style.display = 'none'
+container_start.style.display = 'flex'
+container_start_h3.innerHTML = `<h3>You have given ${correct_answers_given} correct answers out of ${total_answers_given}. Accuracy is ${Math.round(correct_answers_given * 100 / total_answers_given)}%.</h3>`
     }, 10000)
+})
 
 
 for (let i = 0; i < answer_buttons.length; i += 1) {
